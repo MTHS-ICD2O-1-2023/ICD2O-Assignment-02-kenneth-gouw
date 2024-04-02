0e233dabaa9d419b468279c0f0b976acd09c7081
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-04-02 18:04:26 +0000

    sss

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Kenneth
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function findAreaClicked() {
+  const radiusInput = parseInt(document.getElementById("radius-of-circle").value)
+  const area = (radiusInput ** 2) * 3.14
+  document.getElementById("answer").innerHTML = "The area is: " + area.toFixed(2) + " cm<sup>2"
+}
